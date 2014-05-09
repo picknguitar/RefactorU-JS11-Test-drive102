bff908b76aa04bea7fa0b5f0f3a45970760c8930
Nick Walker <nwalker75@yahoo.com>
2014-05-09 13:10:27 -0600

    first commit. test driven development.

---
+++ main.js
@@ -0,0 +1,9 @@
+// $(document).on('ready', function() {
+  
+// });
+
+/** Given a day number from 0 to 6, return the full day of the week for that number. */
+var getDayName = function(dayNumber) {
+	var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	return dayNames[dayNumber];
+};
